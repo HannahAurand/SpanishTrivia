fedb2aaa6737421aa4db1f7fb515eb4410f74ea6
“Hannah <hannahaurand@Hannahs-MacBook-Pro.local>
2018-02-14 09:20:59 -0500

    Added 10 trivia questions in the Qestions Array

--- script.js
+++ script.js
@@ -29,25 +29,170 @@ var ansC = document.querySelector('#answer_C')
 var ansD = document.querySelector('#answer_D')
 
 
+//Storing the user's answer choice in a var using an if statement
+
+function Choice() {
+    if (ansA.addEventListener('click')=true) {
+        Choice = ansA
+    } else if (ansB.addEventListener('click')=true) {
+        Choice = ansB
+    } else if (ansC.addEventListener('click'=true) {
+        Choice = ansC
+    } else if (ansD.addEventListener('click'=true) {
+        Choice = ansD
+    }
+    
+}
+
+var Choice = Choice()
 
 //setting vars for the positions of the girl and the bull
-var girlVert = girl.style.margin-top=//set this by calling a function
-// that changes depending on the right answer or wrong answer
-var girlHorz = girl.style.margin-left=""
-var bullVert = bull.style.margin-top=""
-var bullHorz = bull.style.margin-left=""
+//set this by calling a function that changes depending on 
+// the right answer or wrong answer
 
+var girlVert = girl.style.marginTop
+var girlHorz = girl.style.marginLeft
+var bullVert = bull.style.marginTop
+var bullHorz = bull.style.marginLeft
 
-//if answer is correct, move the girl
-function correct () {
-    if () {}
+//move the girl forward on correct answer
 
- }
+// if (Choice === correctAnswer) {
+//     // do this
 
-//if the answer is wrong, move the bull
-function incorrect () {
-    if () {
+// } else {
+//     // do this
+// }
 
-    }
+// if (Choice === correctAnswer) {
+//     function correctMove () {
+//         parseFloat(girlHorz, girlVert)
+//         girlHorz = girlHorz - 15;
+//         girlVert = girlVert + 30;
+//     } else {
+//         function incorrectMove () {
+//         parseFloat(bullHorz, bullVert)
+//         bullHorz = bullHorz - 15;
+//         bullVert = bullVert + 30;
+//         }
+//     }
+// }
 
+//Function to display the questions and answers in the display
+
+function Display () {
+    /*
+    1. take the current value of the index and display the question
+    at that index in the questions Array.
+
+    2. listen for click event on the question answers lis
+    3. when clicked, compare clicked answer to correct answer
+    4. A. If correct: let the user know they got the right answer
+    4. B. If incorrect: let the user know they got the wrong answer
+    5. set a timeout (i.e. delay) for ~500ms then switch to next question:
+        - check if a next question
+        - if a next question:
+            - increment index number
+            - display the question at the new index
+        - else, display their score or something
+
+    */
 }
+
+//creating a template for the questions and answers class
+class Question {
+    constructor (question, ansA, ansB, ansC, ansD, correctAnswer) {
+        this.question = question
+        this.ansA = ansA
+
+        // CHANGE this
+        ansB = this.ansB
+        ansC = this.ansC
+        ansD = this.ansD
+        correctAnswer = this.correctAnswer
+    }
+}
+
+var Questions = [
+    new Question(
+        "Hello",
+        "Hallo",
+        "Holla",
+        "Hola",
+        "Hi",
+        "Hola"
+    ),
+    new Question (
+        "How are you?",
+        "¿Cómo estás?",
+        "¿Hola?",
+        "Gato",
+        "Perro",
+        "¿Cómo estás?"
+    ),
+    new Question (
+        "Name",
+        "Nombre",
+        "Nominal",
+        "Hambre",
+        "Hombre",
+        "Nombre"
+    ),
+    new Question (
+        "Bottle",
+        "Bebida",
+        "Hambre",
+        "Botella",
+        "Bebé",
+        "Botella"
+    ),
+    new Question (
+        "Water",
+        "Aqua",
+        "Agua",
+        "Mesa",
+        "Queso",
+        "Agua"
+    ),
+    new Question (
+        "Table",
+        "Gato",
+        "Perro",
+        "Mesa",
+        "Queso",
+        "Mesa"
+    ),
+    new Question (
+        "Eat",
+        "Beber",
+        "Comer",
+        "Bailar",
+        "Ir",
+        "Comer"
+    ),
+    new Question (
+        "To sleep",
+        "Siesta",
+        "Dormir",
+        "Comer",
+        "Cantar",
+        "Dormir"
+    ),
+    new Question (
+        "To love",
+        "Odiar",
+        "Amar",
+        "Cantar",
+        "Morir",
+        "Amar"
+    ),
+    new Question (
+        "To forgive",
+        "Odiar",
+        "Perdonar",
+        "Dormir",
+        "Comer",
+        "Perdonar"
+    ),
+]
+
